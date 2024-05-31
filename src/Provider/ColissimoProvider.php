a58d195409c96275cfeae6f55bd68d38d02b55ae
tlenoble <tlenoble@gingerminds.fr>
2024-05-31 15:48:58 +0200

    fix(Bundle) fix

--- src/Provider/ColissimoProvider.php
+++ src/Provider/ColissimoProvider.php
@@ -53,7 +53,18 @@ final class ColissimoProvider extends Provider
                 'encoding' => 'utf-8'
             ]);
 
-            dd($client);
+            dd($client->findRDVPointRetraitAcheminement([
+                "accountNumber" => $this->colissimoAccount,
+                "password" => $this->colissimoPassword,
+                "address" => $address->getStreet(),
+                "zipCode" => $address->getPostcode(),
+                "city" => $address->getCity(),
+                "codTiersPourPartenaire" => $this->colissimoAccount,
+                "countryCode" => $address->getCountryCode(),
+                "weight" => sprintf('%05d', $order->getTotalWeight()),
+                "shippingDate" => $date->format('d/m/Y'),
+                "filterRelay" => 1
+            ]));
 
             $cpPoints = $client->findRDVPointRetraitAcheminement([
                 "accountNumber" => $this->colissimoAccount,
