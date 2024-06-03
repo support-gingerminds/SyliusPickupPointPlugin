75199ae61d87459001a9c21d398f7018cd5a938f
tlenoble <tlenoble@gingerminds.fr>
2024-06-03 09:08:47 +0200

    feature(Bundle) add pickupPointCode value to retrieve picupPoint easier

--- src/Model/PickupPointAwareTrait.php
+++ src/Model/PickupPointAwareTrait.php
@@ -12,6 +12,10 @@ trait PickupPointAwareTrait
     #[ORM\Column(name:"pickup_point_id", type: 'string', nullable: true)]
     protected ?string $pickupPointId = null;
 
+    /** @ORM\Column(name="pickup_point_code", type="string", nullable=true) */
+    #[ORM\Column(name:"pickup_point_code", type: 'string', nullable: true)]
+    protected ?string $pickupPointCode = null;
+
     public function hasPickupPointId(): bool
     {
         return null !== $this->pickupPointId;
@@ -25,5 +29,15 @@ trait PickupPointAwareTrait
     public function getPickupPointId(): ?string
     {
         return $this->pickupPointId;
+    }
+
+    public function setPickupPointCode(?string $pickupPointCode): void
+    {
+        $this->pickupPointCode = $pickupPointCode;
+    }
+
+    public function getPickupPointCode(): ?string
+    {
+        return $this->pickupPointCode;
     }
 }
