b53a6ab2073dcccf45b52dc3c1b280ddcd525b48
tlenoble <tlenoble@gingerminds.fr>
2024-06-03 14:35:55 +0200

    feature(Bundle) fix distance to pickup point model

--- src/Model/PickupPointAwareTrait.php
+++ src/Model/PickupPointAwareTrait.php
@@ -8,13 +8,28 @@ use Doctrine\ORM\Mapping as ORM;
 
 trait PickupPointAwareTrait
 {
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?PickupPoint $pickupPoint = null;
+
     /** @ORM\Column(name="pickup_point_id", type="string", nullable=true) */
-    #[ORM\Column(name:"pickup_point_id", type: 'string', nullable: true)]
+    #[ORM\Column(name:"pickup_point_identifier", type: 'string', nullable: true)]
     protected ?string $pickupPointId = null;
 
     /** @ORM\Column(name="pickup_point_code", type="string", nullable=true) */
     #[ORM\Column(name:"pickup_point_code", type: 'string', nullable: true)]
     protected ?string $pickupPointCode = null;
+
+    public function getPickupPoint(): ?PickupPoint
+    {
+        return $this->pickupPoint;
+    }
+
+    public function setPickupPoint(?PickupPoint $pickupPoint): static
+    {
+        $this->pickupPoint = $pickupPoint;
+
+        return $this;
+    }
 
     public function hasPickupPointId(): bool
     {
